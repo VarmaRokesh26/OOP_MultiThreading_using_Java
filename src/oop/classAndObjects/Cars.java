ecfc7480057bf66dffad5c49b2ab47dab73e57aa
VarmaScary123 <vrokeshvarma@gmail.com>
2024-06-24 01:50:23 +0530

    Class And Objects using Example : Cars

--- src/oop/classAndObjects/Cars.java
+++ src/oop/classAndObjects/Cars.java
@@ -2,7 +2,13 @@ package oop.classAndObjects;
 
 public class Cars {
     public static void main(String arg[]) {
-        DodgeChallenger dodgeChallenger = new DodgeChallenger();
-        dodgeChallenger.startEngine();
-    }
+        DodgeChallenger redDodgeChallenger = new DodgeChallenger("Red", (byte)3, (short)392, (short)492, (short)6700, 30000, 10000, false);
+        redDodgeChallenger.getDescription();
+        redDodgeChallenger.startEngine();
+
+        System.out.println("---------------------------------------------------------------------");
+        DodgeChallenger orangeDodgeChallenger = new DodgeChallenger("Orange", (byte)1, (short)392, (short)492, (short)6700, 30000, 10000, false);
+        orangeDodgeChallenger.getDescription();
+        orangeDodgeChallenger.startEngine();
+    } 
 }
