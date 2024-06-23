1ec13aa188ad312afc0990fcaf595d4b858389e3
VarmaScary123 <vrokeshvarma@gmail.com>
2024-06-24 01:26:29 +0530

    first commit

---
+++ src/oop/classAndObjects/Cars.java
@@ -0,0 +1,8 @@
+package oop.classAndObjects;
+
+public class Cars {
+    public static void main(String arg[]) {
+        DodgeChallenger dodgeChallenger = new DodgeChallenger();
+        dodgeChallenger.startEngine();
+    }
+}
---
+++ src/oop/classAndObjects/DodgeChallenger.java
@@ -0,0 +1,35 @@
+package oop.classAndObjects;
+import java.util.Random;
+
+public class DodgeChallenger {
+
+    private byte numberOfSeats = 5;
+    private byte numberOfDoors = 3;
+    private byte numberOfVehicleOwners = 1;
+    private byte emissionSticker = 4;
+
+    private short power = 362;
+    private short horsePower = 492;
+    private short co2Emission = 333;
+    private short cubicCapacity = 6417;
+
+    private int price = 29999;
+    private int mileage = 14999;
+
+    private long registrationNumber = 134513532145234553L;
+
+    private float fuelConsumptionCombined = 15.5F;
+    private float fuelConsumptionUrban = 21.4F;
+    private float fuelConsumptionExtraUrban = 13.6F;
+
+    private double fuelConsumptionPreciseAverage = 153.2452345234525E-1;
+
+    private boolean isDamaged = true;
+    private char energyEfficiencyCategory = 'G';
+
+    private String color;
+
+    public void startEngine() {
+        System.out.println("The Engine is Started");
+    }
+}
