a76411f3a33ebdbef4eb4ae6181a7f6b0156a200
VarmaScary123 <vrokeshvarma@gmail.com>
2024-07-11 09:45:29 +0530

    Created Items Class for place items in the building for getting robbed

---
+++ src/oop/theGreatRoberyGame/Items.java
@@ -0,0 +1,19 @@
+package oop.theGreatRoberyGame;
+
+public class Items {
+    private String name;
+    private double value;
+
+    public Items(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
