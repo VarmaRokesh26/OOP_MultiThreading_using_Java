14450d5809fbe08fb5abef0abfe1e0e84e86b809
VarmaScary123 <vrokeshvarma@gmail.com>
2024-07-16 12:11:39 +0530

    ToDoList using Vector

---
+++ src/collections/vector/ToDoListVector.java
@@ -0,0 +1,19 @@
+package collections.vector;
+
+import java.util.*;
+public class ToDoListVector {
+    public static void main(String arg[]) {
+        Vector<String> toDoVector = new Vector<>(2, 3);
+        System.out.println("Initial capacity of the vector: " + toDoVector.capacity());
+
+        toDoVector.add("Cleaning the garden");
+        toDoVector.add("Painting the walls");
+        toDoVector.add("Taking the dog for a walk");
+
+        System.out.println("Capacity of the vector after adding 3 elements: " + toDoVector.capacity());
+        System.out.println("Size of the vector after adding 3 elements: " + toDoVector.size());
+
+        System.out.println("The first element of the vector is: " + toDoVector.firstElement());
+        System.out.println("The last element of the vector is: " + toDoVector.lastElement());
+    }
+}
