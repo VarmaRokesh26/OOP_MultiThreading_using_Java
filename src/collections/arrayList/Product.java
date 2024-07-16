bea921a4ea306ec98064a1fd66c218dd03467748
VarmaScary123 <vrokeshvarma@gmail.com>
2024-07-16 11:16:37 +0530

    Collections

---
+++ src/collections/arrayList/Product.java
@@ -0,0 +1,68 @@
+package collections.arrayList;
+
+import java.util.Objects;
+
+public class Product {
+    
+    private String name;
+    private double price;
+    private String description;
+
+    public Product(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(price);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+
+        if(!(obj instanceof Product)) return false;
+
+        Product product = (Product) obj;
+        return Objects.equals(product.name, name) && product.price == price && product.description.equals(description);
+    }
+
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", price=" + price + ", description=" + description + "]";
+    }
+    
+}
