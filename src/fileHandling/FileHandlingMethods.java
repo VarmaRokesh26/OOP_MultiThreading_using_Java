e7f2d2c18db0afa278ef57178e67f41c9c31a6d9
VarmaScary123 <vrokeshvarma@gmail.com>
2024-07-19 10:22:19 +0530

    File Handling methods 1

---
+++ src/fileHandling/FileHandlingMethods.java
@@ -0,0 +1,31 @@
+package fileHandling;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileHandlingMethods {
+    public static void main(String arg[]) {
+        File filePath = new File("src\\fileHandling\\FileHandlingMethods.java");
+
+        System.out.println("File Name                       : " + filePath.getName());
+        System.out.println("Parent                          : " + filePath.getParent());
+        System.out.println("The File exists                 : " + filePath.exists());
+        System.out.println("This file object is a directory : " + filePath.isDirectory());
+        System.out.println("This file Object is File        : " + filePath.isFile());
+        System.out.println("Tihs file path is absolute      : " + filePath.isAbsolute());
+        System.out.println("-----------------------------------------------------------");
+        
+        System.out.println("Can the file Execute            : " + filePath.canExecute());
+        System.out.println("Can read the file               : " + filePath.canRead());
+        System.out.println("Can write in the File           : " + filePath.canWrite());
+        System.out.println("-----------------------------------------------------------");
+        
+        System.out.println("Get Path                        : " + filePath.getPath());
+        System.out.println("get absolute Path               : " + filePath.getAbsolutePath());
+        try {
+            System.out.println("Get canonical Path              : " + filePath.getCanonicalPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
