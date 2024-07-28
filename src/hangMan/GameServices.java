97b77d3b0aeda6e3275a18c4ae0f7b11491e96a0
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-07-28 23:00:10 +0530

    HangMan Game Dev 2

--- src/hangMan/GameServices.java
+++ src/hangMan/GameServices.java
@@ -1,5 +1,25 @@
 package hangMan;
 
-public class GameServices {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class GameServices {
+    public void start() {
+
+    }
+
+    private static List<String> getTheWords() throws FileNotFoundException {
+        List<String> words = new ArrayList<>();
+        Scanner wordsScanner = new Scanner(new File("src\\hangMan\\words.txt"));
+
+        while(wordsScanner.hasNextLine()) {
+            words.add(wordsScanner.nextLine());
+        }
+
+        wordsScanner.close();
+        return words;
+    }
 }
