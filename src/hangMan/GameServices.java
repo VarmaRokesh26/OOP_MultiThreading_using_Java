02524932866923d52f4be6cb6c771290b461cd7a
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-07-28 23:13:56 +0530

    HangMan Game Dev 4

--- src/hangMan/GameServices.java
+++ src/hangMan/GameServices.java
@@ -1,6 +1,7 @@
 package hangMan;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -8,8 +9,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class GameServices {
-    public void start() {
+    public void start() throws FileNotFoundException{
+        String randomWord = getRandomWord();
+        char[] randomWordArray = randomWord.toCharArray();
+        int numberOfChances = randomWord.length();
+        char[] guessingWordArray = new char[numberOfChances];
 
+        Arrays.fill(guessingWordArray, '_');
+
+        System.out.println("Starting the Game Get Ready with your Guesses :)");
+        System.out.println("The length of the word and the number of chances you have is : " + numberOfChances);
+        System.out.println("the word" + new String(guessingWordArray));
+
+        // System.out.println(randomWord);
     }
 
     private static String getRandomWord() throws FileNotFoundException{
--- src/hangMan/HangManApp.java
+++ src/hangMan/HangManApp.java
@@ -1,5 +1,11 @@
 package hangMan;
 
+import java.io.FileNotFoundException;
+
 public class HangManApp {
 
+    public static void main(String[] args) throws FileNotFoundException{
+        GameServices gameServices = new GameServices();
+        gameServices.start();
+    }
 }
