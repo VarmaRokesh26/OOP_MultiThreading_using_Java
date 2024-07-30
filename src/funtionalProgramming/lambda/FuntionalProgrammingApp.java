ef97c59f73f7af7281457635618b4b20b8f4ce8c
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-07-30 09:14:12 +0530

    Functional Programming Part 2

--- src/funtionalProgramming/lambda/FuntionalProgrammingApp.java
+++ src/funtionalProgramming/lambda/FuntionalProgrammingApp.java
@@ -4,16 +4,11 @@ public class FuntionalProgrammingApp {
 
     public static void main(String[] args) {
         WelcomeMessageService welcomeMessageService = new WelcomeMessageService();
-        Greeter greeter;
-        boolean isLoggedInUser = true;
-        if(isLoggedInUser) {
-            greeter = new LoggedInUserGreeter();
-        } else {
-            greeter = new GuestUserGreeter();
-        }
-        welcomeMessageService.greet(greeter);
-
-        MyFunctionType loggedInUserGreeter = ()-> System.out.println("Welcom");
+        Greeter greeter = new GuestUserGreeter();
+        Greeter loggedInUserGreeter = ()-> System.out.println("Welcome Back");
+        welcomeMessageService.greet(()-> System.out.println("Welcome Back"));
+        greeter.perform();
+        loggedInUserGreeter.perform();
     }
 }
 
