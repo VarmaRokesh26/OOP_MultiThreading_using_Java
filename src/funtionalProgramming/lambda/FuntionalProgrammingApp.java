595a4ad23ec1c1212ea49b37b7dd352aa064cf50
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-07-30 09:30:51 +0530

    Functional Programming Part 3

--- src/funtionalProgramming/lambda/FuntionalProgrammingApp.java
+++ src/funtionalProgramming/lambda/FuntionalProgrammingApp.java
@@ -1,14 +1,26 @@
 package funtionalProgramming.lambda;
 
+import java.util.Scanner;
+
 public class FuntionalProgrammingApp {
 
     public static void main(String[] args) {
         WelcomeMessageService welcomeMessageService = new WelcomeMessageService();
-        Greeter greeter = new GuestUserGreeter();
-        Greeter loggedInUserGreeter = ()-> System.out.println("Welcome Back");
-        welcomeMessageService.greet(()-> System.out.println("Welcome Back"));
-        greeter.perform();
-        loggedInUserGreeter.perform();
+
+        Scanner userStatusScanner = new Scanner(System.in);
+        String userStatus = userStatusScanner.nextLine();
+
+        Greeter greeter;
+        if("guest".equals(userStatus)) {
+            greeter = () -> System.out.println("Welcome to our Application. Please create an acccount or log in.");
+        } else if ("loggedIn".equals(userStatus)){
+            greeter = () -> System.out.println("Welocme Back");
+        } else {
+            greeter = () -> System.err.println("Something went Wrong");
+        }
+
+        welcomeMessageService.greet(greeter);
+        userStatusScanner.close();
     }
 }
 
