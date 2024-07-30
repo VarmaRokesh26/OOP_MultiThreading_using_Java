fe0070870ee1be8c214b19b009cd38a0a1914923
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-07-30 17:41:32 +0530

    Strems Basics

---
+++ src/streams/StreamsExample.java
@@ -0,0 +1,17 @@
+package streams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsExample {
+    public static void main(String[] args) {
+        List<Integer> inputNumbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> outputNumbers = inputNumbers
+                .stream()
+                .map(number -> number * number)
+                .filter(value -> value > 3)
+                .collect(Collectors.toList());
+
+        System.out.println(outputNumbers);
+    }
+}
