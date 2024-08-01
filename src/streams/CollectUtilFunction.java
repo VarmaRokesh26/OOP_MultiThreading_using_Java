daddd81b3cc6a6c452633ac8f1af20559636fcd6
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-01 23:28:06 +0530

    .summingInt() & .summingDouble() method in streams

--- src/streams/CollectUtilFunction.java
+++ src/streams/CollectUtilFunction.java
@@ -1,6 +1,5 @@
 package streams;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +8,7 @@ public class CollectUtilFunction {
     public static void main(String[] args) {
         List<String> playerNames = List.of("Usain", "Choppra", "Dhoni", "Rohit");
         List<Integer> playerDamage = List.of(12, 20, 15, 120, 30, 16, 20, 100);
+        List<Double> playerDamageDoubles = List.of(12.0, 20.0, 15.0, 120.0, 30.0, 16.0, 20.0, 100.0);
 
         System.out.println("Player name : " + String.join(", ", playerNames));
         // System.out.println("Player Damage : " + String.join(", ", playerDamage));
@@ -22,5 +22,15 @@ public class CollectUtilFunction {
                 .collect(Collectors.counting());
 
         System.out.println("Number of Hits : " + numberOfHits);
+
+        Integer overAllDamage = playerDamage.stream()
+                .collect(Collectors.summingInt(Integer::intValue));
+
+        System.out.println("Over all Damage : " + overAllDamage);
+
+        Double overAllDamageDouble = playerDamageDoubles.stream()
+                .collect(Collectors.summingDouble(Double::doubleValue));
+
+        System.out.println("Over all Damages : " + overAllDamageDouble);
     }
 }
