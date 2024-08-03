c9d6a4c47f560e3c226cb6249322e51d95101a1b
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-03 16:20:26 +0530

    .reduce()

---
+++ src/streams/StreamsReduce.java
@@ -0,0 +1,28 @@
+package streams;
+import java.util.List;
+
+public class StreamsReduce {
+
+    public static void main(String[] args) {
+
+        List<Item> shoppingCart = List.of(
+                new Item("strawberry", 10, 10),
+                new Item("blueberry", 20, 30),
+                new Item("pear", 10, 30)
+        );
+
+        double subTotal = shoppingCart.stream()
+                .mapToDouble(Item::getPrice)
+                .sum();
+        double discount = -10;
+        double totalPrice = shoppingCart.stream()
+                .map(Item::getPrice)
+                .reduce(discount, Double::sum);
+
+        System.out.println("Subtotal: \t\t$" + subTotal);
+        System.out.println("Discount: \t\t$" + discount);
+        System.out.println("Total price: \t\t$" + totalPrice);
+
+
+    }
+}
