2fae5c1c7a2d3447e362ab693d49bff3976a28f6
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-04 11:12:27 +0530

    .peek()

--- src/streams/StreamsPeek.java
+++ src/streams/StreamsPeek.java
@@ -1,6 +1,7 @@
 package streams;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import streams.ExampleDataDefinition.Item;
 
@@ -13,12 +14,21 @@ public class StreamsPeek {
                 new Item("pear", 10, 28.99),
                 new Item("apple", 10, 3.99),
                 new Item("kiwi", 10, 11.99),
-                new Item("pear", 10, 9.99),
+                new Item("pear", 20, 9.99),
                 new Item("strawberry", 20, 19.99),
                 new Item("apple", 10, 15.99),
                 new Item("apple", 10, 4.99)
         );
 
-
+        List<Item> filteredFruitBoxes = fruitBoxes.stream()
+                .filter(fruitBox -> fruitBox.getQuantity() > 10)
+                .peek(fruitBox -> System.out.println("Quantity filtered :" + fruitBox))
+                .peek(fruitBox -> fruitBox.setPrice(fruitBox.getPrice() + 5))
+                .filter(fruitBox -> fruitBox.getPrice() < 20)
+                .peek(fruitBox -> System.out.println("Price filtered : " + fruitBox))
+                .collect(Collectors.toList());
+        
+        System.out.println("Filtered Fruit Box : ");
+        filteredFruitBoxes.stream().forEach(System.out::println);
     }
 }
