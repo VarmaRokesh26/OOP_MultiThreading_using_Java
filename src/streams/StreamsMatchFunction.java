763f227d5e38697a45a18c0d119932a9189e30bd
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-04 10:19:31 +0530

    .allMatch()

---
+++ src/streams/StreamsMatchFunction.java
@@ -0,0 +1,25 @@
+package streams;
+
+import java.util.List;
+
+public class StreamsMatchFunction {
+
+    public static void main(String[] args) {
+        List<Item> fruitBoxes = List.of(
+            new Item("strawberry", 10, 7.99),
+            new Item("blueberry", 20, 26.99),
+            new Item("pear", 10, 28.99),
+            new Item("apple", 10, 3.99),
+            new Item("kiwi", 10, 11.99),
+            new Item("pear", 10, 9.99),
+            new Item("strawberry", 20, 19.99),
+            new Item("apple", 10, 15.99),
+            new Item("apple", 10, 4.99)
+        );
+
+        boolean isMoreExpensive = fruitBoxes.stream()
+                .allMatch(item -> item.getPrice() > 1);
+
+        System.out.println("All fruit boxes are more expensive than 10$ : " + isMoreExpensive);
+    }
+}
