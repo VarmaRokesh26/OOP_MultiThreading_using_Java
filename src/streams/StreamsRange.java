12280ad23530387789b0cc016466564db87c0ce6
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-04 10:35:07 +0530

    .rangeClosed()

--- src/streams/StreamsRange.java
+++ src/streams/StreamsRange.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 public class StreamsRange {
 
     public static void main(String[] args) {
-        IntStream.range(0, 30)
+        IntStream.rangeClosed(0, 30)
             .forEach(System.out::println);
     }
 }
