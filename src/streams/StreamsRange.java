967dab5e047ae974b7d180baa2d7f51b0ea0c8ea
VarmaRokesh26 <vrokeshvarma@gmail.com>
2024-08-04 10:41:16 +0530

    .range() with type conversion

--- src/streams/StreamsRange.java
+++ src/streams/StreamsRange.java
@@ -5,7 +5,15 @@ import java.util.stream.IntStream;
 public class StreamsRange {
 
     public static void main(String[] args) {
-        IntStream.rangeClosed(0, 30)
+
+        System.out.println("Temperature Scale from 10 to 30 : ");
+        IntStream.rangeClosed(10, 30)
+            .filter(number -> number % 2 == 0)
+            .forEach(System.out::println);
+
+        System.out.println("Possiblity of rain : ");
+        IntStream.range(1, 10).asDoubleStream()
+            .map(number -> number/10)
             .forEach(System.out::println);
     }
 }
